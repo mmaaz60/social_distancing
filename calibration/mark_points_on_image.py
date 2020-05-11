2fa84d2b1fd1302c01bf482e9dfe840bd252a4b6
Muhammad Maaz <mmaaz60@gmail.com>
2020-05-12 02:47:51 +0500

    Some improvements

--- calibration/mark_points_on_image.py
+++ calibration/mark_points_on_image.py
@@ -2,13 +2,6 @@ import numpy as np
 import cv2
 
 
-def mouse_handler(event, x, y, flags, data):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(data['image'], (x, y), 3, (0, 0, 255), 5, 16)
-        cv2.imshow("Image", data['image'])
-        data['points'].append([x, y])
-
-
 class MarkPoints:
     def __init__(self, image):
         self.image = image
@@ -16,13 +9,27 @@ class MarkPoints:
         self.num_points = None
         self.data = {}
 
+    def mouse_handler(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            cv2.circle(self.data['image'], (x, y), 3, (0, 0, 255), 5, 16)
+            cv2.imshow("Image", self.data['image'])
+            self.data['points'].append([x, y])
+
     def mark_points(self, num_points):
         image_copy = self.image_with_points_marked
         self.data['image'] = image_copy
         self.data['points'] = []
 
-        cv2.setMouseCallback("Image", mouse_handler, self.data)
-        cv2.imshow("Image", image_copy)
-        cv2.waitKey(0)
+        cv2.setMouseCallback("Image", self.mouse_handler, self.data)
+        while True:
+            cv2.imshow("Image", image_copy)
+            key = cv2.waitKey(1) & 0xFF
+
+            if key == 13 or key == 141:
+                break
+
+            if key == 27:
+                self.data['points'] = []
+                self.data['image'] = self.image
 
         return np.vstack(self.data['points']).astype(float)
