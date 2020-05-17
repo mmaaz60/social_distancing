2cc39d9e43912db424b3c4664c5be91c5028c888
Muhammad Maaz <mmaaz60@gmail.com>
2020-05-17 17:04:11 +0500

    Fixes the typo

--- image_transformation/transform.py
+++ image_transformation/transform.py
@@ -2,7 +2,7 @@ from config.config import Configuration as config
 import sys
 
 
-class Detector:
+class Transform:
     def __init__(self):
         if config.cfg["image_transformation"] == "homography":
             from image_transformation.homography import Homography as Transformation
@@ -11,5 +11,5 @@ class Detector:
             sys.exit(1)
         self.transform = Transformation()
 
-    def do_inference(self, src_pts, dst_pts):
+    def calculate_transform(self, src_pts, dst_pts):
         return self.transform.calculate_transform(src_pts, dst_pts)
