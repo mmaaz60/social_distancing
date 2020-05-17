89736d38bcce2ea816888b7d62ccfc5f789c44c2
Muhammad Maaz <mmaaz60@gmail.com>
2020-05-17 17:45:25 +0500

    Resize the image to best fit into the scree

--- calibration/transform_camera_view.py
+++ calibration/transform_camera_view.py
@@ -6,7 +6,7 @@ from image_transformation.transform import Transform
 
 class TransformCameraView:
     def __init__(self, camera_view_image):
-        self.camera_image = camera_view_image
+        self.camera_image = cv2.resize(camera_view_image, (640, 360))
         self.top_view_size = (360, 360, 3)
         self.top_view = np.zeros(self.top_view_size, np.uint8)
         self.camera_view_points = self.top_view_points = None
@@ -14,11 +14,11 @@ class TransformCameraView:
         self.transformed_image = None
 
     def mark_points_on_camera_view_image(self, num_points):
-        mark_points = MarkPoints(self.camera_image)
+        mark_points = MarkPoints(self.camera_image, "Camera View")
         self.camera_view_points = mark_points.mark_points(num_points)
 
     def mark_points_on_top_view(self, num_points):
-        mark_points = MarkPoints(self.top_view)
+        mark_points = MarkPoints(self.top_view, "Top View")
         self.top_view_points = mark_points.mark_points(num_points)
 
     def calculate_transformation(self):
